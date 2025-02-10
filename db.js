7a07d93947469089c88a5fd96cc2e5b5fea0f134
endxkartikey <endxkartikey@gmail.com>
2025-02-10 22:09:24 +0530

    Updated code for Bitespeed assignment

--- db.js
+++ db.js
@@ -8,7 +8,12 @@ if (!process.env.DB_URL) {
 
 const sequelize = new Sequelize(process.env.DB_URL, {
   dialect: "postgres",
-  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 module.exports = sequelize;
